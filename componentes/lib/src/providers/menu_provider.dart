5a6465326c50f29f7271970ec46dd5a175d23cd8
Edgardo Rafael Yoliani Cantillo <edrayoca@outlook.es>
2021-04-13 18:19:18 -0500

    Future Builder e iconos

--- componentes/lib/src/providers/menu_provider.dart
+++ componentes/lib/src/providers/menu_provider.dart
@@ -10,10 +10,10 @@ class _MenuProvider {
 
   Future<List<dynamic>> cargarData() async {
     final resp = await rootBundle.loadString('data/menu_opts.json');
+
     Map dataMap = json.decode(resp);
-    //print(data);
-    print(dataMap['rutas']);
     opciones = dataMap['rutas'];
+
     return opciones;
   }
 }
