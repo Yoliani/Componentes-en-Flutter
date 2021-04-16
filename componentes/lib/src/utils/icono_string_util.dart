fe149eb56b4853ed0bbe8f280705d172b2c1a857
Edgardo Rafael Yoliani Cantillo <edrayoca@outlook.es>
2021-04-16 18:49:46 -0500

    Cambiando uno de los String (folder_open)

--- componentes/lib/src/utils/icono_string_util.dart
+++ componentes/lib/src/utils/icono_string_util.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 final _icons = <String, IconData>{
   'add_alert': Icons.add_alert,
   'accessibility': Icons.accessibility,
-  'accessibility': Icons.folder_open,
+  'folder_open': Icons.folder_open,
 };
 
 Icon getIcon(String nombreIcono) {
