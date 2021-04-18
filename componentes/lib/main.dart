d9cc4b64b00465f6f76b07339dbb86611e6bd265
Edgardo Rafael Yoliani Cantillo <edrayoca@outlook.es>
2021-04-18 16:20:05 -0500

    Rutas generadas dinámicamente y

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,9 +1,10 @@
-//import 'package:componentes/src/pages/home_temp.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+//Paquete de material
 import 'package:flutter/material.dart';
 
 import 'src/pages/alert_page.dart';
-import 'src/pages/home_page.dart';
+
+//Rutas
+import 'src/routes/routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,10 +16,11 @@ class MyApp extends StatelessWidget {
       title: 'Componentes APP',
       //home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext) => HomePage(),
-        'alert': (BuildContext) => AlertPage(),
-        'avatar': (BuildContext) => AvatarPage(),
+      routes: getAppRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        print('Ruta llamada: ${settings.name}');
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
---
+++ componentes/lib/src/routes/routes.dart
@@ -0,0 +1,14 @@
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+final rutas = <String, WidgetBuilder>{
+  '/': (BuildContext context) => HomePage(),
+  'alert': (BuildContext context) => AlertPage(),
+  'avatar': (BuildContext context) => AvatarPage(),
+};
+
+Map<String, WidgetBuilder> getAppRoutes() {
+  return rutas;
+}
