4bca199a904084e5d26b2d4999d424afc3d06718
Edgardo Rafael Yoliani Cantillo <edrayoca@outlook.es>
2021-04-18 16:07:32 -0500

    Navegar con rutas con nombre

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,6 +1,8 @@
 //import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:flutter/material.dart';
 
+import 'src/pages/alert_page.dart';
 import 'src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +13,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes APP',
-      home: HomePage(),
+      //home: HomePage(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext) => HomePage(),
+        'alert': (BuildContext) => AlertPage(),
+        'avatar': (BuildContext) => AvatarPage(),
+      },
     );
   }
 }
